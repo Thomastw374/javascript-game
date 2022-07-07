6fe7060121d9a04e4e21cbd2d7943556d6e451c8
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-07 14:58:19 +0100

    added some assets, html, styling and game window

---
+++ script.js
@@ -0,0 +1,25 @@
+// Browser based game. Obstacle run type game.
+
+// Create basic html
+
+// Game window contained within a div
+
+// objects in the game will be html elements I suppose
+
+// Gonna need a position variable for the character
+
+// program movements to occur on keypresses, key presses manipulate position variables
+
+// appearance of floor moving can be achieved in an animated way, swapping out imgs
+
+// change image file on keypresses to animate character
+
+// limit the variables so that you can't clip through the floor
+
+// At semi random intervals render an enemy/obstacle on the right side of screen
+
+// floor will be animated via cycling of images to depict movement
+
+// Got to get assets. Can use free ones for now and swap out for Alfies at a later date
+
+// animation should all be set at same framerate
