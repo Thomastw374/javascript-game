c8f630aa2ec2c19d83f3a39cf2a7a773dc25e44a
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-07 18:03:30 +0100

    added ground animation

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const characterSprite = document.querySelector(".main__game-character");
+const floorSprite = document.querySelector(".main__game-floor");
 
-
-let i = 0
+let i = 0;
 function animateCharacter() {
-    characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`
-    if (i<5) {
-        i += 1
-    } else if (i=5) {
-        i -= 5
-    }
+  characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`;
+  if (i < 5) {
+    i += 1;
+  } else if ((i = 5)) {
+    i -= 5;
+  }
 }
 
-setInterval(animateCharacter, 100)
+setInterval(animateCharacter, 100);
+
+let j = 0;
+
+function animateFloor() {
+  floorSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/floor-animation/floor${j}.png`;
+  if (j < 3) {
+    j += 1;
+  } else if ((j = 3)) {
+    j -= 3;
+  }
+}
+
+setInterval(animateFloor, 50);
