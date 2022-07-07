9eecc50ffa36edc095903a1fcc91444fa4330c03
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-07 17:45:50 +0100

    implemented running animation

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const characterSprite = document.querySelector(".main__game-character");
 let i = 0
 function animateCharacter() {
     characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`
-    if (i<6) {
+    if (i<5) {
         i += 1
-    } else if (i=6) {
-        i -= 6
+    } else if (i=5) {
+        i -= 5
     }
 }
 
-setInterval(animateCharacter(), 1000)
+setInterval(animateCharacter, 100)
