1f1b1cd16194fb0c5234e3a7de0b2754c97ce4cb
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-07 17:35:04 +0100

    positioned character correctly, began work on js

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-// Browser based game. Obstacle run type game.
+const characterSprite = document.querySelector(".main__game-character");
 
-// Create basic html
 
-// Game window contained within a div
+let i = 0
+function animateCharacter() {
+    characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`
+    if (i<6) {
+        i += 1
+    } else if (i=6) {
+        i -= 6
+    }
+}
 
-// objects in the game will be html elements I suppose
-
-// Gonna need a position variable for the character
-
-// program movements to occur on keypresses, key presses manipulate position variables
-
-// appearance of floor moving can be achieved in an animated way, swapping out imgs
-
-// change image file on keypresses to animate character
-
-// limit the variables so that you can't clip through the floor
-
-// At semi random intervals render an enemy/obstacle on the right side of screen
-
-// floor will be animated via cycling of images to depict movement
-
-// Got to get assets. Can use free ones for now and swap out for Alfies at a later date
-
-// animation should all be set at same framerate
+setInterval(animateCharacter(), 1000)
