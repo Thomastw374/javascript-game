bac97d443f6c277ed210ed4c66c5e623c475973e
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-12 11:41:00 +0100

    found method for manipulating characer height in percentages

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 const characterSprite = document.querySelector(".main__game-character");
 const floorSprite = document.querySelector(".main__game-floor");
-let charPositionY = document.querySelector(".main__character-container").style
-  .bottom;
+let charPositionY = document.getElementById("game-character");
 
-// let i = 0;
-// function animateCharacter() {
-//   characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`;
-//   if (i < 5) {
-//     i += 1;
-//   } else if ((i = 5)) {
-//     i -= 5;
+console.log(getComputedStyle(charPositionY).getPropertyValue('bottom'))
+
+// since these are the COMPUTED styles you'll get back the bottom style in px
+
+// function jumpIfElse() {
+//   if (k < 10) {
+//     charPositionY = 33.5 + k + "%";
+//   } else if (k > 10) {
+//     charPositionY = 43.5 - k + "%";
 //   }
 // }
 
-// setInterval(animateCharacter, 100);
-
-// let j = 0;
-
-// function animateFloor() {
-//   floorSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/floor-animation/floor${j}.png`;
-//   if (j < 3) {
-//     j += 1;
-//   } else if ((j = 3)) {
-//     j -= 3;
-//   }
+// let k = 0;
+// const handleJump = (event) => {
+//   console.log("event triggered")
+//   setInterval(jumpIfElse, 1000)
+  
 // }
 
-// setInterval(animateFloor, 50);
 
-let k = 0;
+// document.addEventListener("keypress", handleJump);
+
+let k = 0
 const handleJump = (event) => {
   console.log("event triggered")
-  setTimeout()
-  for (k = 0; k < 20; k++) {
-    if (k < 10) {
-      charPositionY = 33.5 + k + "%";
-    } else if (k > 10) {
-      charPositionY = 43.5 - k + "%";
-    }
+  console.log(charPositionY)
+  
+  
+
+  // charPositionY.style.setProperty("bottom", 33.5 + 20 + "%");
+}
+
+function jumpIfElse() {
+  if (k < 10) {
+    charPositionY.style.setProperty("bottom", 33.5 + k + "%");
+  } else if (k > 10) {
+    charPositionY.style.setProperty("bottom", 43.5 - (k - 10) + "%");
+  } else if ((k = 20)) {
+    k -= 20;
   }
-};
+}
+
+setInterval(jumpIfElse, 1000);
 
 document.addEventListener("keypress", handleJump);
