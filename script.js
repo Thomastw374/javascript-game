64e703c0444b9361ef7cf6bed50b85afc844c304
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-12 15:05:10 +0100

    added cycling of different obstacle types

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const characterSprite = document.querySelector(".main__game-character");
+const characterContainer = document.querySelector(".main__character-container");
 const floorSprite = document.querySelector(".main__game-floor");
-const characterContainer = document.querySelector(".main__character-container")
+const obstacleContainer = document.querySelector(".main__obstacle-container")
+const obstacleSprite = document.querySelector(".main__game-obstacle")
 
+// cycles through the sprites.
 let i = 0;
 function animateCharacter() {
   characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`;
@@ -13,6 +16,26 @@ function animateCharacter() {
 }
 
 let charRunTimer = setInterval(animateCharacter, 100);
+
+let l = 0;
+
+function animateObstacles() {
+  obstacleSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/obstacles/${l}.png`;
+  obstacleContainer.id = "animate-obstacle"
+  // also have a different class for each item corresponding to the correct css. Because pics come in at diff heights need to change css.
+  setTimeout(function () {
+    obstacleContainer.id = "";
+  }, 900);
+  l += 1
+  console.log(l)
+  if (l == 6) {
+    l -= 6
+  }
+}
+
+setInterval(animateObstacles, 1200)
+
+
 
 let j = 0;
 
@@ -27,26 +50,20 @@ function animateFloor() {
 
 setInterval(animateFloor, 50);
 
-
 const handleJump = (event) => {
-  console.log("event triggered")
-  clearInterval(animateCharacter)
-  characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/run-animation/tile00${i}.png`;
-  
+  console.log("event triggered");
+  clearInterval(animateCharacter);
+  characterSprite.src = `./craftpix-net-798594-free-green-zone-tileset-pixel-art/Assets/jump-animation/jump0.png`;
+
   if (characterContainer.id != "animate-jump") {
-    characterContainer.id = "animate-jump"
+    characterContainer.id = "animate-jump";
   }
-  setTimeout(function(){
-    characterContainer.id = ""}, 
-    500
-  )
+  setTimeout(function () {
+    characterContainer.id = "";
+  }, 500);
+};
 
-
-}
-
-
-document.addEventListener("keydown", function() {
+document.addEventListener("keydown", function () {
   charRunTimer = clearInterval(animateCharacter);
-  handleJump() 
-})
-
+  handleJump();
+});
